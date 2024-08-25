3e4d9df8a6f35a701652bc81aef30fc3da4003ce
kapil5444 <nerpagarkapil@gmail.com>
2024-08-26 01:25:49 +0530

    Re-added basicvar.js

---
+++ basicvar.js
@@ -0,0 +1,25 @@
+// const accountId = 1444553
+// let accountEmail = "kapil.com"
+// var accountPassword = "12345"
+// accountCity = "Pune"
+// let accountState;
+
+// // accountId = 2
+
+
+
+// accountEmail = "manoj.com"
+// accountPassword = "12435"
+// accountCity = "nashik"
+
+// console.log(accountId);
+// console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+const accountId = "14666"
+let accountEmail = "makdjdh.com"
+var accountCity = "pune"
+accountPassword = "1566"
+let accountState;
+
+console.table([accountId,accountEmail,accountCity,accountPassword,accountState])
