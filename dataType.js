93e2bf8b60f938e0441ddbd25e2feaa7abf820e9
kapil5444 <nerpagarkapil@gmail.com>
2024-08-26 22:25:28 +0530

    dataType

---
+++ dataType.js
@@ -0,0 +1,10 @@
+"use strict";
+let name = "kapil"
+let age = 23
+let isLoggedin = true
+
+// number => 2 to power 52
+// String = ""
+// bigint = 
+console.log(typeof"kapil");
+console.log(typeof age)
